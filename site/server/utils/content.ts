d2e328e72a7031e5fa5c9c7c9f6b69bbb5f4c947
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-09-20 18:53:02 +0200

    refactor: rename folder

--- site/server/utils/content.ts
+++ site/server/utils/content.ts
@@ -2,5 +2,5 @@ import { comarkContent } from 'comark-content';
 import fs from 'comark-content/sources/fs';
 
 export const content = comarkContent({
-  source: fs('../content'),
+  source: fs('../policies'),
 });
